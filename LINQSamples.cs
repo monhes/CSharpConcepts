7e89144de3557c4965727b0dbc5bf07d6e75b8e0
Thud Bloodseeker <antibody13@hotmail.com>
2021-04-23 13:55:56 +0700

    Add project files.

---
+++ LINQSamples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CSharpConcepts
+{
+    class LINQSamples
+    {
+        public int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        public void LessThanFive()
+        {
+            var lowNums = from num in numbers
+                          where num < 5
+                          select num;
+
+            Console.WriteLine("Numbers < 5:");
+            foreach (var x in lowNums)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpConcepts
+{
+    public enum SomeRootVegetable
+    {
+        HorseRadish,
+        Radish,
+        Turnip
+    }
+
+    [Flags]
+    public enum Seasons
+    {
+        None = 0,
+        Summer = 1,
+        Autumn = 2,
+        Winter = 4,
+        Spring = 8,
+        All = Summer | Autumn | Winter | Spring
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputstring = Console.ReadLine();
+            LINQSamples OBJLinqSamples = new LINQSamples();
+            if(inputstring == "linq"){
+                OBJLinqSamples.LessThanFive();
+                Console.WriteLine("LINQ DONE");
+            }
+            else if(inputstring == "generic"){
+
+            }
+        }
+    }
+
+    public class Location : COORD
+    {
+        private string locationName; //Location class field
+
+        public Location(string name) => Name = name; //constructor with expression
+
+        public string Name
+        {
+            get => locationName;
+            set => locationName = value;
+        }
+    }
+
+    public class COORD
+    {
+        public int X;
+        public int Y;
+        public int ShowCord()
+        {
+            int sum = X + Y;
+            return sum;
+        }
+    }
+}
