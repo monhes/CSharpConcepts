cc78ec23c117586ee8a711280fecd45500892539
Thud Bloodseeker <antibody13@hotmail.com>
2021-05-01 15:37:19 +0700

    OOP Class fields , properties , static ,Constructor ,inheri

--- Program.cs
+++ Program.cs
@@ -26,16 +26,100 @@ namespace CSharpConcepts
     {
         static void Main(string[] args)
         {
-            var inputstring = Console.ReadLine();
-            LINQSamples OBJLinqSamples = new LINQSamples();
-            if(inputstring == "linq"){
-                OBJLinqSamples.LessThanFive();
-                Console.WriteLine("LINQ DONE");
-            }
-            else if(inputstring == "generic"){
+            Import(); // Constructor import Sutudent
+            var student = new List<Student>(); 
+            var OBJLinqSamples = new LINQSamples();
 
+
+
+            var menu = Console.ReadLine();
+            switch (menu)
+            {
+                case "linq":
+                    OBJLinqSamples.LessThanFive();
+                    Console.WriteLine("LINQ DONE");
+                    break;
+
+                case "cg": //class generic collection
+                    var insStudent = new List<Student>(); // instance Student class collections
+                    bool adding = true; 
+                    while (adding)
+                    {
+                        var ObjStudent = new Student();
+                        Console.WriteLine("name : ");
+                        ObjStudent.Name = Console.ReadLine();
+                        Console.WriteLine("grade : ");
+                        ObjStudent.Grade = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("phone : ");
+                        ObjStudent.Phone = (Convert.ToInt32(Console.ReadLine()));
+
+                        insStudent.Add(ObjStudent); // add to last list<>
+                        Student.Count++;
+                        Console.WriteLine("Student count : {0}" , Student.Count);
+
+                        Console.WriteLine("-- add more ? -- ");
+                        if (Console.ReadLine() != "y")
+                        {
+                            adding = false;
+                        }
+
+                    }
+                    foreach (var item in insStudent)
+                    {
+                        Console.WriteLine(item.Name);
+                        Console.WriteLine(item.Grade);
+                        Console.WriteLine("***");
+                    }
+                    break;
+
+                case "generic":
+                    break;
+                default:
+                    break;
             }
+
+            
+
         }
+
+        static void Import()
+        {
+            var importedStudent = new Student("thu",2,81);
+            Console.WriteLine(importedStudent.Name);
+        }
+    }
+    public class Member
+    {
+        public string Name;
+        protected int phone; //switch to protected instead private
+        public int Phone //migrated from Student class
+        {
+            set { phone = value; }
+        }
+    }
+
+    public class Student : Member
+    {
+        static public int Count = 0;
+        //public string Name;
+        public int Grade;
+        //public int phone;
+        public Student()
+        {
+        }
+        public Student(string paraName, int paraGrade, int paraPhone)
+        {
+            Name = paraName;
+            Grade = paraGrade;
+            phone = paraPhone;
+        }
+    }
+
+    public class Teacher : Member
+    {
+        //public string Name;
+        public string Subject;
+        //private int Phone;
     }
 
     public class Location : COORD
